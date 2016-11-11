15353e30186e232e25aa5f222e418fdaa6bb7a37
Andrew <andrew.jia.2013@gmail.com>
2016-11-11 15:01:08 -0800

    paco

---
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TacoTeleOp.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+public class TacoTeleOp extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    TacoTeleOp   robot           = new TacoTeleOp();              // Use a K9'shardware
+    double          armPosition     = robot.ARM_HOME;                   // Servo safe position
+    double          clawPosition    = robot.CLAW_HOME;                  // Servo safe position
+    final double    CLAW_SPEED      = 0.01 ;                            // sets rate to move servo
+    final double    ARM_SPEED       = 0.01 ;
+    public void runOpMode() {
+        double front;
+        double back;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+            front = -gamepad1.left_stick_y;
+            back = -gamepad1.right_stick_y;
+            robot.frontMotor.setPower(front);
+            robot.backMotor.setPower(back);
+
+            // Use gamepad Y & A raise and lower the arm
+            if (gamepad1.a)
+                armPosition += ARM_SPEED;
+            else if (gamepad1.y)
+                armPosition -= ARM_SPEED;
+
+            // Use gamepad X & B to open and close the claw
+            if (gamepad1.x)
+                clawPosition += CLAW_SPEED;
+            else if (gamepad1.b)
+                clawPosition -= CLAW_SPEED;
+
+            // Move both servos to new position.
+            armPosition  = Range.clip(armPosition, robot.ARM_MIN_RANGE, robot.ARM_MAX_RANGE);
+            robot.arm.setPosition(armPosition);
+            clawPosition = Range.clip(clawPosition, robot.CLAW_MIN_RANGE, robot.CLAW_MAX_RANGE);
+            robot.claw.setPosition(clawPosition);
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("arm",   "%.2f", armPosition);
+            telemetry.addData("claw",  "%.2f", clawPosition);
+            telemetry.addData("front",  "%.2f", front);
+            telemetry.addData("back", "%.2f", back);
+            telemetry.update();
+        }
+    }
+}
